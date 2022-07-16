d6c78827ee06b038133b285e12806a81add4347a
pei-hsiu-li <pei-hsiu.li.1@ens.etsmtl.ca>
2022-07-15 23:57:34 -0400

    add partie1

--- AuthApp/routes/web.php
+++ AuthApp/routes/web.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
-
- 
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,10 +21,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+\Illuminate\Support\Facades\Auth::routes();
+//Partie 1
+/* 
+Route::get('/auth/{provider}/redirect', function ($provider) {
+    return Socialite::driver($provider)->redirect();
+})->where('provider', 'github|google');
+
+Route::get('/auth/{provider}/callback', function ($provider) {
+    $user = Socialite::driver($provider)->user();
+
+    $user = \App\Models\User::where([
+        'provider' => $provider,
+        'provider_id' => $user->getId()
+    ])->first();
+
+    if(!$user) {
+        $validator = \Illuminate\Support\Facades\Validator::make(
+            ['email' => $user->getEmail()],
+            ['email' => ['unique:user,email']],
+            ['email.unique' => 'Failed']
+        );
+        if ($validator->fails()){
+            return redirect('/login')->withErrors($validator);
+        }
+    }
+    $user = \App\Models\User::create([
+        'name' => $user->getName(),
+        'email' => $user->getEmail(),
+        'provider' => $provider,
+        'provider_id' => $user->getId(),
+        'email_verified_at' => now()
+    ]);
+    \Illuminate\Support\Facades\Auth::login($user);
+    return redirect('/');
+})->where('provider', 'github|google');
+ */
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+//Partie 2
 Route::get('/login', function(Request $request){
     $request->session()->put("state", $state = Str::random(40));
     $query = http_build_query([
@@ -55,8 +88,6 @@ Route::get('/callback', function(Request $request){
     ]);
     return $response->json();
 });
-
-
 
 Route::middleware([
     'auth:sanctum',
